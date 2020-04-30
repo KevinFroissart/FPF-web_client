a0d2de3e1a0716967330cb644cff5bb0f9224b5a
julienmichot <julien.michot@hotmail.fr>
2020-04-30 11:59:32 +0200

    création des formulaires de connexion et inscription

--- src/main.js
+++ src/main.js
@@ -6,3 +6,26 @@ const appContainer = document.querySelector('#appContainer'),
 fetch(apiUrl + '/api/v1/myresource')
 	.then(response => response.text())
 	.then(message => (appContainer.innerHTML = message));
+
+const connectionForm = document.querySelector('.connection');
+const registrationForm = document.querySelector('.registration');
+
+connectionForm.style.display = 'initial';
+
+function toggleConnectionMethod(event) {
+	event.preventDefault();
+	const target = event.target;
+
+	if (target.innerText == "S'inscrire") {
+		connectionForm.style.display = 'none';
+		registrationForm.style.display = 'initial';
+	} else if (target.innerText == 'Se connecter') {
+		registrationForm.style.display = 'none';
+		connectionForm.style.display = 'initial';
+	}
+}
+
+const connectionLinks = document.querySelectorAll('#basket button');
+connectionLinks.forEach(link =>
+	link.addEventListener('click', toggleConnectionMethod)
+);
