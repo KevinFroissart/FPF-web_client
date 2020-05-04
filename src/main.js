d1a312c7d2fc3a2e855c2fbe9fa4839a22d8dd72
julienmichot <julien.michot@hotmail.fr>
2020-05-04 13:14:55 +0200

    fonction étiquette remise

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import getApiUrl from './utils/url';
-import * as badge from './utils/badge';
+import badge from './utils/badge';
 
 const appContainer = document.querySelector('#appContainer'),
 	apiUrl = getApiUrl(window.location);
@@ -8,7 +8,9 @@ fetch(apiUrl + '/api/v1/myresource')
 	.then(response => response.text())
 	.then(message => (appContainer.innerHTML = message));
 
-/* Allows to switch between connection and registration forms **/
+/**
+ *  Allows to switch between connection and registration forms
+ */
 const connectionForm = document.querySelector('.connection');
 const registrationForm = document.querySelector('.registration');
 
@@ -30,4 +32,26 @@ function toggleConnectionMethod(event) {
 const connectionLinks = document.querySelectorAll('#basket button');
 connectionLinks.forEach(link =>
 	link.addEventListener('click', toggleConnectionMethod)
+);
+
+/**
+ * Display text area when the corresponding checkBox is checked
+ */
+function displayTextArea(event) {
+	const checkBox = event.target;
+	const textArea = document.querySelector(`.${this.id}Area`);
+
+	/* If the checkbox is checked, display the output text */
+	if (checkBox.checked == true) {
+		textArea.style.display = 'initial';
+	} else {
+		textArea.style.display = 'none';
+	}
+}
+
+const checkBox = document.querySelectorAll(
+	".personnalisation input[type='checkbox']"
+);
+checkBox.forEach(checkBox =>
+	checkBox.addEventListener('click', displayTextArea)
 );
