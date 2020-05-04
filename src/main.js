6976adf6cadc2010be8eb1b3207a8b839bdd8e07
Aymeric Hénouille <aymeric.henouille@gmail.com>
2020-05-04 12:56:51 +0200

    fix: remove item

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import getApiUrl from './utils/url';
-import $ from 'jquery';
+import * as badge from './utils/badge';
 
 const appContainer = document.querySelector('#appContainer'),
 	apiUrl = getApiUrl(window.location);
@@ -31,43 +31,3 @@ const connectionLinks = document.querySelectorAll('#basket button');
 connectionLinks.forEach(link =>
 	link.addEventListener('click', toggleConnectionMethod)
 );
-
-/* Display text area when the corresponding checkBox is checked */
-function displayTextArea(event) {
-	const checkBox = event.target;
-	const textArea = document.querySelector(`.${this.id}Area`);
-
-	// If the checkbox is checked, display the output text
-	if (checkBox.checked == true) {
-		textArea.style.display = 'initial';
-	} else {
-		textArea.style.display = 'none';
-	}
-}
-
-const checkBox = document.querySelectorAll(
-	".personnalisation input[type='checkbox']"
-);
-checkBox.forEach(checkBox =>
-	checkBox.addEventListener('click', displayTextArea)
-);
-
-const choiceList = document.querySelector('.choiceList');
-const listAjouts = [];
-
-function reloadChoiceList() {
-	choiceList.innerHTML = '';
-	listAjouts.forEach(badge => (choiceList.innerHTML += badge));
-}
-
-function dropdownChoice(event) {
-	event.preventDefault();
-	const item = event.target;
-	const badge = `<a href="#" class="badge badge-success" >${item.innerText}</a>`;
-	listAjouts.push(badge);
-	reloadChoiceList();
-}
-const items = document.querySelectorAll('.dropdownChoice .dropdown-item');
-items.forEach(item => item.addEventListener('click', dropdownChoice));
-
-$('.badge').click(() => $(this).remove());
---
+++ src/utils/badge.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+
+/**
+ * Ajoute des items sur la page de personalisation des faire-part de mariage.
+ */
+$(() => {
+
+    /**
+     * Ajoute un item depuis la liste
+     */
+    $('.dropdownChoice .dropdown-menu .dropdown-item').click((e) => {
+        const item = $(e.target).html();
+        $('.choiceList').append(`<a href="#" class="badge badge-success" >${item}</a>`);
+    });
+
+    /**
+     * Supprime l'item quand on clique dessus.
+     */
+    $('.choiceList').on('click', '.badge', (e) => $(e.target).remove());
+
+});
