bff9f5d64cc66eb0522a4db28375881c46dc2b56
Aymeric Hénouille <aymeric.henouille@gmail.com>
2020-05-04 23:42:50 +0200

    fix: server url

--- src/utils/url.js
+++ src/utils/url.js
@@ -3,7 +3,7 @@ const url = 'groupe9.azae.eu';
 export default function getApiUrl({ protocol, port }) {
 	const isProd = port === '';
 	const apiPort = isProd ? '' : ':8080';
-	const hostname = '192.168.99.100';
+	const hostname = url;
 
 	return `${protocol}//${hostname}${apiPort}`;
 }
