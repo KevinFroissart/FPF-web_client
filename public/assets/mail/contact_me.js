3504ca5f678da851a0838b624b7539dede64cbed
Kevin Froissart <kevin.froissart69@gmail.com>
2020-05-07 17:06:45 +0200

    fix: chemin api

--- public/assets/mail/contact_me.js
+++ public/assets/mail/contact_me.js
@@ -17,7 +17,7 @@ $(function () {
 			$this = $('#sendMessageButton');
 			$this.prop('disabled', true); // Disable submit button until AJAX call is complete to prevent duplicate messages
 			$.ajax({
-				url: `https://groupe9.azae.eu:8080/api/v1/contact/query`,
+				url: ` http://groupe9.azae.eu/api/contact/query`,
 				type: 'POST',
 				data: {
 					name: name,
